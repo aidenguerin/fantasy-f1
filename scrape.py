d4a867214a22adb6bef4c224b46d19814fc4c2ae
Aiden Guerin <90363998+aidenguerin@users.noreply.github.com>
2024-03-14 12:31:36 +1100

    Functions to call the fantasy f1 api for official asset stats

--- scrape.py
+++ scrape.py
@@ -9,9 +9,25 @@ def write_round_summary(path, json):
         with open(path, 'w') as f:
             json.dump(json, f)
 
+def print_json(data):
+    print(json.dumps(data, indent=4))
+
+def get_asset_ids(round_summary):
+    assets = data["Data"]["Value"]
+    asset_ids = []
+    for asset in assets:
+        asset_ids.append(asset["PlayerId"])
+
+    return asset_ids
+
+def get_asset_stats(asset_id):
+    r = requests.get(f"https://fantasy.formula1.com/feeds/popup/playerstats_{asset_id}.json")
+    return r.status_code, r.json()
+
 ROUND_NUMBER = 1        
 PATH = f"data/round_summary/{ROUND_NUMBER}.json"
 status_code, data = get_round_summary(ROUND_NUMBER)
 
-print(json.dumps(data, indent=4))
+status_code, data = get_asset_stats(210)
+print_json(data)
 
